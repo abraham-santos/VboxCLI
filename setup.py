495914633403b6f9f81b746f6bc64a1b9284d2a5
abraham-santos <abraham.santos@live.com>
2021-01-24 13:44:56 -0400

    modifying setup file

---
+++ setup.py
@@ -0,0 +1,43 @@
+"""Instalador para el paquete "vboxcli"."""
+
+from setuptools import setup
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    open('LICENSE').read()
+    + '\n')
+
+setup(
+    name="vboxcli",
+    version="0.1",
+    description="Command Line Interface for VirtualBox API.",
+    long_description="",
+    #
+    classifiers=[
+        # ¿Cuan maduro esta este proyecto? Valores comunes son
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 3 - Alpha",
+        # Indique a quien va dirigido su proyecto
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        # Indique licencia usada (debe coincidir con el "license")
+        "License :: OSI Approved :: MIT License",
+        # Indique versiones soportas, Python 2, Python 3 o ambos.
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+    ],
+    keywords="ejemplo instalador paquete vboxcli",
+    author="Abraham Santos",
+    author_email="aesantosterrazas@gmail.com",
+    url="https://github.com/abraham-santos/VboxCLI",
+    download_url="https://github.com/abraham-santos/VboxCLI.git",
+    license="MIT",
+    platforms="Unix",
+    packages=["vboxcli"],
+    include_package_data=True,
+)
